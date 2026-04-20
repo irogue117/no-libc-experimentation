5e2631c6dc98d46527934bddb997d4086963fc2f
irogue117 <117@mail.irogue.xyz>
2026-04-20 07:47:56 +0200

    testing signing

--- src/stack_protector.c
+++ src/stack_protector.c
@@ -6,7 +6,8 @@
 typedef __SIZE_TYPE__ size_t;
 
 __attribute__((noreturn)) void __stack_chk_fail(void) {
-  static const char msg[] = "stack smashing detected EXITING ALL PROCESS THREADS\n";
+  static const char msg[] =
+      "stack smashing detected EXITING ALL PROCESS THREADS\n";
 
   // Best effort (try, but not important)
   syscall3(SYS_write, 2, (long)msg, sizeof(msg) - 1);
@@ -14,7 +15,8 @@ __attribute__((noreturn)) void __stack_chk_fail(void) {
   // Exit all threads in process.
   syscall1(SYS_exit_group, 127);
 
-  // Tells compiler to do whatever it can to kill the process e.g run illegal assembly command.
+  // Tells compiler to do whatever it can to kill the process e.g run illegal
+  // assembly command.
   __builtin_trap();
   for (;;)
     ;
