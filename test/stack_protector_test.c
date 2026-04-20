13bdf38b0123adfa8798c45f1da7541adf450b72
Alpharius <flibberjib.uneaten066@passmail.net>
2026-04-20 06:51:41 +0200

    Fixed stack_protector_test

--- test/stack_protector_test.c
+++ test/stack_protector_test.c
@@ -1,15 +1,11 @@
-#include "../src/stack_protector.h"
+#include "stack_protector.h"
 
-void smash(void)
-{
-    char buf[8];
+void smash(void) {
+  char buf[8];
 
-    for (size_t i = 0; i < 64; ++i) {
-        buf[i] = 'A';
-    }
+  for (size_t i = 0; i < 64; ++i) {
+    buf[i] = 'A';
+  }
 }
 
-
-void _start(void) {
-    smash();
-}
+void _start(void) { smash(); }
